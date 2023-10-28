d79203afa165706b2b1cf2c9ba5b20331fb917d2
SHIN <akiyachiumi@gmail.com>
2023-10-29 01:01:52 +0800

    add vercel setting

--- indexl.js
+++ indexl.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3001");
+app.listen(port, () => {
+  console.log("Server is running.");
 });
